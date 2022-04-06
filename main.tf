c9bf6d46115b7f23f65ea29f626842e94245400a
Eduardo Gomes <74272550+eduardogomes-tech@users.noreply.github.com>
2022-04-06 19:26:52 -0300

    Atividade Terraform

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_virtual_network" "vnet-atividade-terraform" {
  resource "azurerm_subnet" "sub-atividade-terraform" {
   name                 = "sub-atividade-terraform"
   resource_group_name = azurerm_resource_group.rg-atividade-terraform.name
-  virtual_network_name = azurerm_virtual_network.vnet-atividade-terraform
+  virtual_network_name = azurerm_virtual_network.vnet-atividade-terraform.name
   address_prefixes     = ["10.0.1.0/24"]
   
 }
